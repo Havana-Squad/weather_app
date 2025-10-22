34e06b3174c6d811fe1ca1a7b74f60bf7412c401
Farah315 <fara734isgreat@gmail.com>
2025-10-23 01:21:01 +0300

    feat: add UI res and themes for weather app

---
+++ lib/presentation/screen/weather_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'sabahoooooo',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
