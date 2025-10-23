61cef6be31e2a6b7748827fe047a612bba7effdd
Farah315 <fara734isgreat@gmail.com>
2025-10-23 19:39:22 +0300

    feat: Add FlutterGen for asset management

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,513 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/arrow_down.svg
+  SvgGenImage get arrowDown =>
+      const SvgGenImage('assets/images/arrow_down.svg');
+
+  /// File path: assets/images/arrow_up.svg
+  SvgGenImage get arrowUp => const SvgGenImage('assets/images/arrow_up.svg');
+
+  /// File path: assets/images/day_clear_sky.png
+  AssetGenImage get dayClearSky =>
+      const AssetGenImage('assets/images/day_clear_sky.png');
+
+  /// File path: assets/images/day_depositing_rime_fog.png
+  AssetGenImage get dayDepositingRimeFog =>
+      const AssetGenImage('assets/images/day_depositing_rime_fog.png');
+
+  /// File path: assets/images/day_drizzle_intensity.png
+  AssetGenImage get dayDrizzleIntensity =>
+      const AssetGenImage('assets/images/day_drizzle_intensity.png');
+
+  /// File path: assets/images/day_drizzle_light.png
+  AssetGenImage get dayDrizzleLight =>
+      const AssetGenImage('assets/images/day_drizzle_light.png');
+
+  /// File path: assets/images/day_drizzle_moderate.png
+  AssetGenImage get dayDrizzleModerate =>
+      const AssetGenImage('assets/images/day_drizzle_moderate.png');
+
+  /// File path: assets/images/day_fog.png
+  AssetGenImage get dayFog => const AssetGenImage('assets/images/day_fog.png');
+
+  /// File path: assets/images/day_freezing_drizzle_intensity.png
+  AssetGenImage get dayFreezingDrizzleIntensity =>
+      const AssetGenImage('assets/images/day_freezing_drizzle_intensity.png');
+
+  /// File path: assets/images/day_freezing_drizzle_light.png
+  AssetGenImage get dayFreezingDrizzleLight =>
+      const AssetGenImage('assets/images/day_freezing_drizzle_light.png');
+
+  /// File path: assets/images/day_freezing_heavy.png
+  AssetGenImage get dayFreezingHeavy =>
+      const AssetGenImage('assets/images/day_freezing_heavy.png');
+
+  /// File path: assets/images/day_freezing_loght.png
+  AssetGenImage get dayFreezingLoght =>
+      const AssetGenImage('assets/images/day_freezing_loght.png');
+
+  /// File path: assets/images/day_mainly_clear.png
+  AssetGenImage get dayMainlyClear =>
+      const AssetGenImage('assets/images/day_mainly_clear.png');
+
+  /// File path: assets/images/day_overcast.png
+  AssetGenImage get dayOvercast =>
+      const AssetGenImage('assets/images/day_overcast.png');
+
+  /// File path: assets/images/day_partly_cloudy.png
+  AssetGenImage get dayPartlyCloudy =>
+      const AssetGenImage('assets/images/day_partly_cloudy.png');
+
+  /// File path: assets/images/day_rain_intensity.png
+  AssetGenImage get dayRainIntensity =>
+      const AssetGenImage('assets/images/day_rain_intensity.png');
+
+  /// File path: assets/images/day_rain_moderate.png
+  AssetGenImage get dayRainModerate =>
+      const AssetGenImage('assets/images/day_rain_moderate.png');
+
+  /// File path: assets/images/day_rain_shower_moderate.png
+  AssetGenImage get dayRainShowerModerate =>
+      const AssetGenImage('assets/images/day_rain_shower_moderate.png');
+
+  /// File path: assets/images/day_rain_shower_slight.png
+  AssetGenImage get dayRainShowerSlight =>
+      const AssetGenImage('assets/images/day_rain_shower_slight.png');
+
+  /// File path: assets/images/day_rain_shower_violent.png
+  AssetGenImage get dayRainShowerViolent =>
+      const AssetGenImage('assets/images/day_rain_shower_violent.png');
+
+  /// File path: assets/images/day_rain_slight.png
+  AssetGenImage get dayRainSlight =>
+      const AssetGenImage('assets/images/day_rain_slight.png');
+
+  /// File path: assets/images/day_snow_fall_intensity.png
+  AssetGenImage get daySnowFallIntensity =>
+      const AssetGenImage('assets/images/day_snow_fall_intensity.png');
+
+  /// File path: assets/images/day_snow_fall_light.png
+  AssetGenImage get daySnowFallLight =>
+      const AssetGenImage('assets/images/day_snow_fall_light.png');
+
+  /// File path: assets/images/day_snow_fall_moderate.png
+  AssetGenImage get daySnowFallModerate =>
+      const AssetGenImage('assets/images/day_snow_fall_moderate.png');
+
+  /// File path: assets/images/day_snow_grains.png
+  AssetGenImage get daySnowGrains =>
+      const AssetGenImage('assets/images/day_snow_grains.png');
+
+  /// File path: assets/images/day_snow_shower_heavy.png
+  AssetGenImage get daySnowShowerHeavy =>
+      const AssetGenImage('assets/images/day_snow_shower_heavy.png');
+
+  /// File path: assets/images/day_snow_shower_slight.png
+  AssetGenImage get daySnowShowerSlight =>
+      const AssetGenImage('assets/images/day_snow_shower_slight.png');
+
+  /// File path: assets/images/day_thunderstrom_slight_or_moderate.png
+  AssetGenImage get dayThunderstromSlightOrModerate => const AssetGenImage(
+    'assets/images/day_thunderstrom_slight_or_moderate.png',
+  );
+
+  /// File path: assets/images/day_thunderstrom_with_heavy_hail.png
+  AssetGenImage get dayThunderstromWithHeavyHail =>
+      const AssetGenImage('assets/images/day_thunderstrom_with_heavy_hail.png');
+
+  /// File path: assets/images/day_thunderstrom_with_slight_hail.png
+  AssetGenImage get dayThunderstromWithSlightHail => const AssetGenImage(
+    'assets/images/day_thunderstrom_with_slight_hail.png',
+  );
+
+  /// File path: assets/images/fast_wind.svg
+  SvgGenImage get fastWind => const SvgGenImage('assets/images/fast_wind.svg');
+
+  /// File path: assets/images/humidity.svg
+  SvgGenImage get humidity => const SvgGenImage('assets/images/humidity.svg');
+
+  /// File path: assets/images/location.svg
+  SvgGenImage get location => const SvgGenImage('assets/images/location.svg');
+
+  /// File path: assets/images/night_clear_sky.png
+  AssetGenImage get nightClearSky =>
+      const AssetGenImage('assets/images/night_clear_sky.png');
+
+  /// File path: assets/images/night_depositing_rime_fog.png
+  AssetGenImage get nightDepositingRimeFog =>
+      const AssetGenImage('assets/images/night_depositing_rime_fog.png');
+
+  /// File path: assets/images/night_drizzle_intensity.png
+  AssetGenImage get nightDrizzleIntensity =>
+      const AssetGenImage('assets/images/night_drizzle_intensity.png');
+
+  /// File path: assets/images/night_drizzle_light.png
+  AssetGenImage get nightDrizzleLight =>
+      const AssetGenImage('assets/images/night_drizzle_light.png');
+
+  /// File path: assets/images/night_drizzle_moderate.png
+  AssetGenImage get nightDrizzleModerate =>
+      const AssetGenImage('assets/images/night_drizzle_moderate.png');
+
+  /// File path: assets/images/night_fog.png
+  AssetGenImage get nightFog =>
+      const AssetGenImage('assets/images/night_fog.png');
+
+  /// File path: assets/images/night_freezing_drizzle_intensity.png
+  AssetGenImage get nightFreezingDrizzleIntensity =>
+      const AssetGenImage('assets/images/night_freezing_drizzle_intensity.png');
+
+  /// File path: assets/images/night_freezing_drizzle_light.png
+  AssetGenImage get nightFreezingDrizzleLight =>
+      const AssetGenImage('assets/images/night_freezing_drizzle_light.png');
+
+  /// File path: assets/images/night_freezing_heavy.png
+  AssetGenImage get nightFreezingHeavy =>
+      const AssetGenImage('assets/images/night_freezing_heavy.png');
+
+  /// File path: assets/images/night_freezing_light.png
+  AssetGenImage get nightFreezingLight =>
+      const AssetGenImage('assets/images/night_freezing_light.png');
+
+  /// File path: assets/images/night_mainly_clear.png
+  AssetGenImage get nightMainlyClear =>
+      const AssetGenImage('assets/images/night_mainly_clear.png');
+
+  /// File path: assets/images/night_overcast.png
+  AssetGenImage get nightOvercast =>
+      const AssetGenImage('assets/images/night_overcast.png');
+
+  /// File path: assets/images/night_partly_cloudy.png
+  AssetGenImage get nightPartlyCloudy =>
+      const AssetGenImage('assets/images/night_partly_cloudy.png');
+
+  /// File path: assets/images/night_rain_intensity.png
+  AssetGenImage get nightRainIntensity =>
+      const AssetGenImage('assets/images/night_rain_intensity.png');
+
+  /// File path: assets/images/night_rain_moderate.png
+  AssetGenImage get nightRainModerate =>
+      const AssetGenImage('assets/images/night_rain_moderate.png');
+
+  /// File path: assets/images/night_rain_shower_moderate.png
+  AssetGenImage get nightRainShowerModerate =>
+      const AssetGenImage('assets/images/night_rain_shower_moderate.png');
+
+  /// File path: assets/images/night_rain_shower_slight.png
+  AssetGenImage get nightRainShowerSlight =>
+      const AssetGenImage('assets/images/night_rain_shower_slight.png');
+
+  /// File path: assets/images/night_rain_shower_violent.png
+  AssetGenImage get nightRainShowerViolent =>
+      const AssetGenImage('assets/images/night_rain_shower_violent.png');
+
+  /// File path: assets/images/night_rain_slight.png
+  AssetGenImage get nightRainSlight =>
+      const AssetGenImage('assets/images/night_rain_slight.png');
+
+  /// File path: assets/images/night_snow_fall_intensity.png
+  AssetGenImage get nightSnowFallIntensity =>
+      const AssetGenImage('assets/images/night_snow_fall_intensity.png');
+
+  /// File path: assets/images/night_snow_fall_light.png
+  AssetGenImage get nightSnowFallLight =>
+      const AssetGenImage('assets/images/night_snow_fall_light.png');
+
+  /// File path: assets/images/night_snow_fall_moderate.png
+  AssetGenImage get nightSnowFallModerate =>
+      const AssetGenImage('assets/images/night_snow_fall_moderate.png');
+
+  /// File path: assets/images/night_snow_grains.png
+  AssetGenImage get nightSnowGrains =>
+      const AssetGenImage('assets/images/night_snow_grains.png');
+
+  /// File path: assets/images/night_snow_shower_heavy.png
+  AssetGenImage get nightSnowShowerHeavy =>
+      const AssetGenImage('assets/images/night_snow_shower_heavy.png');
+
+  /// File path: assets/images/night_snow_shower_slight.png
+  AssetGenImage get nightSnowShowerSlight =>
+      const AssetGenImage('assets/images/night_snow_shower_slight.png');
+
+  /// File path: assets/images/night_thunderstrom_slight_or_moderate.png
+  AssetGenImage get nightThunderstromSlightOrModerate => const AssetGenImage(
+    'assets/images/night_thunderstrom_slight_or_moderate.png',
+  );
+
+  /// File path: assets/images/night_thunderstrom_with_heavy_hail.png
+  AssetGenImage get nightThunderstromWithHeavyHail => const AssetGenImage(
+    'assets/images/night_thunderstrom_with_heavy_hail.png',
+  );
+
+  /// File path: assets/images/night_thunderstrom_with_slight_hail.png
+  AssetGenImage get nightThunderstromWithSlightHail => const AssetGenImage(
+    'assets/images/night_thunderstrom_with_slight_hail.png',
+  );
+
+  /// File path: assets/images/pressure.svg
+  SvgGenImage get pressure => const SvgGenImage('assets/images/pressure.svg');
+
+  /// File path: assets/images/rain.svg
+  SvgGenImage get rain => const SvgGenImage('assets/images/rain.svg');
+
+  /// File path: assets/images/temperature.svg
+  SvgGenImage get temperature =>
+      const SvgGenImage('assets/images/temperature.svg');
+
+  /// File path: assets/images/uv.svg
+  SvgGenImage get uv => const SvgGenImage('assets/images/uv.svg');
+
+  /// List of all assets
+  List<dynamic> get values => [
+    arrowDown,
+    arrowUp,
+    dayClearSky,
+    dayDepositingRimeFog,
+    dayDrizzleIntensity,
+    dayDrizzleLight,
+    dayDrizzleModerate,
+    dayFog,
+    dayFreezingDrizzleIntensity,
+    dayFreezingDrizzleLight,
+    dayFreezingHeavy,
+    dayFreezingLoght,
+    dayMainlyClear,
+    dayOvercast,
+    dayPartlyCloudy,
+    dayRainIntensity,
+    dayRainModerate,
+    dayRainShowerModerate,
+    dayRainShowerSlight,
+    dayRainShowerViolent,
+    dayRainSlight,
+    daySnowFallIntensity,
+    daySnowFallLight,
+    daySnowFallModerate,
+    daySnowGrains,
+    daySnowShowerHeavy,
+    daySnowShowerSlight,
+    dayThunderstromSlightOrModerate,
+    dayThunderstromWithHeavyHail,
+    dayThunderstromWithSlightHail,
+    fastWind,
+    humidity,
+    location,
+    nightClearSky,
+    nightDepositingRimeFog,
+    nightDrizzleIntensity,
+    nightDrizzleLight,
+    nightDrizzleModerate,
+    nightFog,
+    nightFreezingDrizzleIntensity,
+    nightFreezingDrizzleLight,
+    nightFreezingHeavy,
+    nightFreezingLight,
+    nightMainlyClear,
+    nightOvercast,
+    nightPartlyCloudy,
+    nightRainIntensity,
+    nightRainModerate,
+    nightRainShowerModerate,
+    nightRainShowerSlight,
+    nightRainShowerViolent,
+    nightRainSlight,
+    nightSnowFallIntensity,
+    nightSnowFallLight,
+    nightSnowFallModerate,
+    nightSnowGrains,
+    nightSnowShowerHeavy,
+    nightSnowShowerSlight,
+    nightThunderstromSlightOrModerate,
+    nightThunderstromWithHeavyHail,
+    nightThunderstromWithSlightHail,
+    pressure,
+    rain,
+    temperature,
+    uv,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = false;
+
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  _svg.SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    _svg.SvgTheme? theme,
+    _svg.ColorMapper? colorMapper,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final _svg.BytesLoader loader;
+    if (_isVecFormat) {
+      loader = _vg.AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = _svg.SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+        colorMapper: colorMapper,
+      );
+    }
+    return _svg.SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter:
+          colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
