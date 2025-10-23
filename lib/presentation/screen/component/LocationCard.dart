fcb26489cf67eb508ea0be8102a06066484519fb
Farah315 <fara734isgreat@gmail.com>
2025-10-23 13:55:01 +0300

    feat: add LocationCard component

---
+++ lib/presentation/screen/component/LocationCard.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LocationCard extends StatelessWidget {
+  final String locationName;
+  final EdgeInsetsGeometry? padding;
+  final AlignmentGeometry? alignment;
+
+  const LocationCard({
+    super.key,
+    this.locationName = 'Khan Younis',
+    this.padding,
+    this.alignment,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textColor = Theme.of(context).colorScheme.onSurface;
+
+    return Container(
+      padding: padding,
+      alignment: alignment,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'assets/images/location.svg',
+            width: 24,
+            height: 24,
+            colorFilter: ColorFilter.mode(
+              textColor,
+              BlendMode.srcIn,
+            ),
+          ),
+          const SizedBox(width: 4),
+          Text(
+            locationName,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
