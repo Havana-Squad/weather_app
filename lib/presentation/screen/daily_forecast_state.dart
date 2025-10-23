8c7f4c2be60bfe9c1def6aa8bd233ed9751c22ea
Farah315 <fara734isgreat@gmail.com>
2025-10-23 19:56:45 +0300

    Update dummy daily forecast data

--- lib/presentation/screen/daily_forecast_state.dart
+++ lib/presentation/screen/daily_forecast_state.dart
@@ -14,8 +14,31 @@ class DailyForecastUiState {
   });
 }
 
-// Dummy data based on the provided image
 List<DailyForecastUiState> dummyDailyForecast = [
+  DailyForecastUiState(
+    day: 'Thursday',
+    forecastImage: Assets.images.dayClearSky,
+    lowTemperature: 20,
+    highTemperature: 32,
+  ),
+  DailyForecastUiState(
+    day: 'Friday',
+    forecastImage: Assets.images.dayOvercast,
+    lowTemperature: 20,
+    highTemperature: 32,
+  ),
+  DailyForecastUiState(
+    day: 'Saturday',
+    forecastImage: Assets.images.dayDrizzleModerate,
+    lowTemperature: 19,
+    highTemperature: 31,
+  ),
+  DailyForecastUiState(
+    day: 'Sunday',
+    forecastImage: Assets.images.dayDrizzleIntensity,
+    lowTemperature: 18,
+    highTemperature: 30,
+  ),
   DailyForecastUiState(
     day: 'Monday',
     forecastImage: Assets.images.dayClearSky,
@@ -30,20 +53,8 @@ List<DailyForecastUiState> dummyDailyForecast = [
   ),
   DailyForecastUiState(
     day: 'Wednesday',
-    forecastImage: Assets.images.dayDrizzleIntensity,
-    lowTemperature: 20,
-    highTemperature: 32,
-  ),
-  DailyForecastUiState(
-    day: 'Thursday',
-    forecastImage: Assets.images.dayClearSky,
-    lowTemperature: 20,
-    highTemperature: 32,
-  ),
-  DailyForecastUiState(
-    day: 'Friday',
-    forecastImage: Assets.images.dayOvercast,
-    lowTemperature: 20,
-    highTemperature: 32,
+    forecastImage: Assets.images.dayDepositingRimeFog,
+    lowTemperature: 21,
+    highTemperature: 33,
   ),
 ];
