71f277b1d68754d84ce1ebd7a634ab5c396b6505
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-10-25 21:50:05 +0300

    refactor: improve location handling in WeatherCubit and update dependencies

--- lib/presentation/cubit/weather_cubit.dart
+++ lib/presentation/cubit/weather_cubit.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:domain/entity/Weather.dart';
-import 'package:domain/repository/weather_repository.dart';
-import 'package:domain/repository/location_repository.dart';
+import 'package:weather_app/domain/entity/Weather.dart';
+import 'package:weather_app/domain/repository/location_repository.dart';
+import 'package:weather_app/domain/repository/weather_repository.dart';
 
 part 'weather_state.dart';
 
@@ -14,65 +14,26 @@ class WeatherCubit extends Cubit<WeatherState> {
 
   StreamSubscription<Position>? _locationSub;
 
-  WeatherCubit(
-      this._weatherRepository,
-      this._locationRepository,
-      ) : super(const WeatherState());
+  WeatherCubit(this._weatherRepository, this._locationRepository)
+    : super(const WeatherState());
 
   Future<void> start() async {
     emit(state.copyWith(isLoading: true, error: null));
-
-    try {
-      final pos = await _locationRepository.getCurrentLocation();
-      if (pos == null) {
-        emit(state.copyWith(isLoading: false, error: 'Location not available'));
-        return;
-      }
-
-      final city = await _getCityName(pos);
-
-      final weather = await _weatherRepository.getWeather(
-        latitude: pos.latitude,
-        longitude: pos.longitude,
-      );
-
-      emit(state.copyWith(
-        position: pos,
-        cityName: city,
-        weather: weather,
-        isLoading: false,
-      ));
-
-      _locationSub = _locationRepository
-          .getLocationStream(accuracy: LocationAccuracy.high)
-          .listen((pos) async {
-        final city = await _getCityName(pos);
-        final weather = await _weatherRepository.getWeather(
-          latitude: pos.latitude,
-          longitude: pos.longitude,
+    _locationSub = _locationRepository
+        .getLocationStream(accuracy: LocationAccuracy.high)
+        .listen(
+          (pos) async {
+            emit(state.copyWith(isLoading: true, error: null));
+            final weather = await _weatherRepository.getWeather(
+              latitude: pos.latitude,
+              longitude: pos.longitude,
+            );
+            emit(state.copyWith(weather: weather, isLoading: false, error: null),);
+          },
+          onError: (e) {
+            emit(state.copyWith(isLoading: false, error: e.toString()));
+          },
         );
-        emit(state.copyWith(
-          position: pos,
-          cityName: city,
-          weather: weather,
-        ));
-      });
-    } catch (e) {
-      emit(state.copyWith(isLoading: false, error: e.toString()));
-    }
-  }
-
-  Future<String?> _getCityName(Position pos) async {
-    try {
-      final placemarks =
-      await placemarkFromCoordinates(pos.latitude, pos.longitude);
-      if (placemarks.isNotEmpty) {
-        return placemarks.first.locality ??
-            placemarks.first.subAdministrativeArea ??
-            placemarks.first.country;
-      }
-    } catch (_) {}
-    return null;
   }
 
   @override
